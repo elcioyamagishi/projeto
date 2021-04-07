c653ab4a11249e0d99fd28d23df74a14f34b9056
murilo-cassiani <murilo.cassiani@gmail.com>
2021-04-07 17:09:49 -0300

    model para receber o período (ínicio e fim)

---
+++ BackEnd/code/src/main/java/com/coffee/code/model/BetweenDate.java
@@ -0,0 +1,23 @@
+package com.coffee.code.model;
+
+import java.util.Date;
+
+public class BetweenDate {
+    private Date from, to;
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+}
