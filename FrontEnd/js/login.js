511314ec188c02988c05151c7f1c0e7e274a2106
GustavoOliveira <gustavodantasdeoliveira@outlook.com>
2021-04-08 19:14:56 -0300

    commit mudanças front

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,42 +1,40 @@
-function autenticar(event) {
-    evento.preventDefault(); //cancela o envio do formulario
+function autenticar(evento) {
+    evento.preventDefault(); // cancela o envio do formulário
 
     let user = document.getElementById("txtUser").value
     let senha = document.getElementById("txtSenha").value
-
+    
     let usuario = {
-        email: user,
-        racf: user,
-        senha: senha // montando o objetivo << agora precisamos colocar no corpo da mensagem
-
+        email:user,
+        racf:user,
+        senha:senha
     }
-
+    
     let msg = {
-        method: 'POST', //declarando qual método utilizaremos no login
-        body: JSON.stringify(usuario),// colocando o que terá no corpo da mensagem. Temos que transformar o objetivo em js em texto (ir em formato JSON através do stringify)
-        headers: {
-            'Content-type': 'application/json' // ou seja, construimos um objeto e o corpo da msg que enviaremos para o backend
+        method: 'POST',
+        body: JSON.stringify(usuario), // transforma o objeto usuario em text json
+        headers: { 
+            'Content-type':'application/json'
         }
     }
 
-    fetch("http://localhost:8080/usuarios/login", msg)
-        .then(res => tratarResposta(res)) // chamada async - comando para enviar para o backend. then() é promessa
-
+    fetch("http://localhost:8080/usuarios/login",msg) 
+            .then(res => tratarResposta(res))
 }
 
-function tratarResposta(resposta) {
-   if(resposta.status !=200){
-       document.getElementById("msgError").innerHTML = "Usuário/Senha invalido(a)"
-   }else{
-       resposta.json().then(res => fazerLogin(res)) // só traz as informações de dentro do usuário
 
-   }
-    
+function tratarResposta(resposta) {
+    if(resposta.status != 200) {
+        document.getElementById("msgError").innerHTML = "Usuario/Senha invalido"
+    }else{
+        resposta.json().then(res => fazerLogin(res))
+    }
+    console.log(resposta)
+
 }
 
 function fazerLogin(user) {
-    localStorage.setItem("userLogged", JSON.stringify(user))
-    window.location = "menu.html" 
-    
-}
+        localStorage.setItem("userlogged", JSON.stringify(user))
+        window.location = "dashmenu.html"
 
+}
