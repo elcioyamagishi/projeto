d091ee6f4002832b0ef071e2adc6338b003711ba
Gabriel Souza Murizine <987313554@itaud.des.ihf>
2021-04-09 08:56:32 -0300

    atualizando arquivos js

--- FrontEnd/JS/menu.js
+++ FrontEnd/JS/menu.js
@@ -1,43 +1,44 @@
 function validaLogin() {
-    let userTxt = localStorage.getItem("userlogged");
+    let userTxt = localStorage.getItem("userLogged");
 
-    if(! userTxt) {
+    if (!userTxt) {
         window.location = "index.html"
     }
 
     let user = JSON.parse(userTxt);
-
-    document.getElementById("user").innerHTML = `nome: ${user.nome} RACF ${user.racf}`
-    document.getElementById("foto").innerHTML = `<img src= ${user.linkFoto}>`
+    document.getElementById("user").innerHTML = `Bem-vindo(a), ${user.nome} (RACF: ${user.racf})!`
+    document.getElementById("foto").innerHTML = `<img src=${user.linkFoto} width=30% height=30%>`
 }
 
-function logout() {
-    localStorage.removeItem("userlogged");
-    window.location = "index.html"
+function logout(){
+    localStorage.removeItem("userLogged");
+    window.location = "index.html";
 }
 
 function gerarRelatorio() {
-    let dataInicio = document.getElementById("dataInicio").value 
-    let dataFinal = document.getElementById("dataFinal").value 
+   // let dataInicio = document.getElementById("dataInicio").value
+   // let dataFinal = document.getElementById("dataFinal").value
+    
+   
+   fetch("http://localhost:8080/usuarios/all")
+    .then(res => tratarResposta(res))
 
-    fetch("http://localhost:8080/usuarios/all")
-        .then(res => tratarResposta(res))
-    console.log(dataInicio,dataFinal)
 }
 
-function tratarResposta(resposta) {
-    if(resposta) {
-        resposta.json().then(res => exibirDados(res))
-    }
-}
+ function tratarResposta(resposta) {
+     if(resposta){
+   resposta.json().then(res => exibirDados(res))
+     }
+     
+ }   
 
-function exibirDados(listaUsers) {
-    let tabela = '<table> <tr><th>nome</th><th>email</th> <th>racf</th></tr>'
-    console.log(listaUsers)
-    for (i=0; i < listaUsers.length; i++) {
-        tabela = tabela + `<tr>  <td>${listaUsers[i].nome}</td> <td>${listaUsers[i].email}</td> <td>${listaUsers[i].racf}</td></tr>`
+ function exibirDados(listaUsers) {
+    let tabela = '<table class = table table-sm> <tr> <th>nome</th> <th>email</th> <th>RACF</th>  </tr>'
+
+    for(i=0; i < listaUsers.length; i++) {
+        tabela = tabela + `<tr> <td>${listaUsers[i].nome}</td>  <td>${listaUsers[i].email}</td> <td>${listaUsers[i].racf}</td> </tr>`
     }
-  
+
     tabela = tabela + '</table>'
     document.getElementById("tabela").innerHTML = tabela
 }
